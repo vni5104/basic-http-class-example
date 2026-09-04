615a84ff9996357f92db94e929fc3cd8156c8c7f
vni5104 <vickyn5104@gmail.com>
2026-09-04 14:45:03 -0400

    refactored server.js and responses.js

--- src/responses.js
+++ src/responses.js
@@ -3,28 +3,26 @@ const fs = require('fs');
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const client2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
 
-const sendPage = (request, response, page) => {
-    response.writeHead(200, {'Content-Type':'text/html'});
-    response.write(page);
+const style = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+
+const serveFile = (request, response, content, mimeType) => {
+    response.writeHead(200, {'Content-Type':mimeType});
+    response.write(content);
     response.end();
 };
 
-const getMessage = (request, response) => {
-    response.writeHead(200, {'Content-Type':'text/plain'});
-    response.write('Hello World');
-    response.end();
-};
+const getMessage = (request, response) => serveFile(request, response, 'Hello World', 'text/plain');
 
-const getIndex = (request, response) => {
-    sendPage(request, response, index)
-};
+const getIndex = (request, response) => serveFile(request, response, index, 'text/html');
 
-const getClient2 = (request, response) => {
-    sendPage(request, response, client2)
-};
+const getClient2 = (request, response) => serveFile(request, response, client2, 'text/html');
+
+const getCSS = (request, response) => serveFile(request, response, style, 'text/css');
 
 module.exports = {
     getIndex,
     getClient2,
-    getMessage
+    getMessage,
+    getCSS
 };
--- src/server.js
+++ src/server.js
@@ -6,15 +6,20 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const onRequest = (request, response) => {
     console.log(request.url);
 
-    if (request.url === '/page2') {
-        responses.getClient2(request, response);
-    } else if (request.url === '/message') {
-        responses.getMessage(request, response);
-    } else {
-        responses.getIndex(request, response);
+    switch (request.url) {
+        case '/page2':
+            responses.getClient2(request, response);
+            break;
+        case '/message':
+            responses.getMessage(request, response);
+            break;
+        case '/style.css':
+            responses.getCSS(request, response);
+            break;
+        default:
+            responses.getIndex(request, response);
+            break;
     }
-
-    //responses.getIndex(request, response);
 };
 
 http.createServer(onRequest).listen(port, () => {
